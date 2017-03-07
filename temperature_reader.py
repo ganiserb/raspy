f447d766c55f5f9c731aedd2370e26db4c8bf683
Gabriel <ganiserb@gmail.com>
2017-03-07 00:18:52 -0300

    Only record changes

--- temperature_reader.py
+++ temperature_reader.py
@@ -5,9 +5,13 @@ from sqlalchemy.orm import sessionmaker
 from models import TemperatureMeasurement, Base
 
 
-sensors = {
-    'red': '/sys/bus/w1/devices/28-800000282522/w1_slave',
-}
+sensors = [
+    {
+        'name': 'red',
+        'path': '/sys/bus/w1/devices/28-800000282522/w1_slave',
+        'last_measurement': None,
+    },
+]
 
 # Set up the DB
 engine = create_engine('sqlite:///test.db', echo=True)
@@ -16,21 +20,29 @@ session = Session()
 # Create the tables if they don't exist
 Base.metadata.create_all(engine)
 
+
+def get_temperature(path):
+    # Returns the temperature given the path of the device
+    with open(path) as device:
+        reading = device.read()
+    data = reading.split('\n')[1].split(" ")[9]
+    return float(data[2:]) / 1000
+
 iteration = 0
 while 1:
     iteration += 1
-    for name, path in sensors.items():
-        with open(path) as device:
-            reading = device.read()
-        data = reading.split('\n')[1].split(" ")[9]
-        temperature = float(data[2:]) / 1000
-        print(name, temperature)
-        measurement = TemperatureMeasurement(
-            device=name,
-            moment=datetime.now(),
-            value=temperature)
-        # Add to DB session
-        session.add(measurement)
+    for sensor in sensors:
+        temperature = get_temperature(sensor['path'])
+
+        if sensor['last_measurement'] != temperature:
+            sensor['last_measurement'] = temperature  # Update value
+            # Only record changes in measurements
+            measurement = TemperatureMeasurement(
+                device=sensor['name'],
+                moment=datetime.now(),
+                value=temperature)
+            # Add to DB session
+            session.add(measurement)
 
     if iteration == 60:
         # Save to DB every 60 measurements
