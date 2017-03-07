1778cb849906415f34d3bebeea6ba7a2bc964ed4
Gabriel <ganiserb@gmail.com>
2017-03-06 23:58:53 -0300

    Added DB recording

---
+++ models.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime
+
+Base = declarative_base()
+
+
+class TemperatureMeasurement(Base):
+    __tablename__ = 'temperature_measurement'
+    id = Column(Integer, primary_key=True)
+    device = Column(String)
+    moment = Column(DateTime)
+    value = Column(Float)
+
+    def __repr__(self):
+        return "TemperatureMeasurement(device={}, moment={}, value={})>".format(
+            self.device, self.moment, self.value)
--- temperature_reader.py
+++ temperature_reader.py
@@ -1,10 +1,35 @@
 import time
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import TemperatureMeasurement, Base
+
+
+sensors = {
+    'red': '/sys/bus/w1/devices/28-800000282522/w1_slave',
+}
+
+# Set up the DB
+engine = create_engine('sqlite:///test.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+# Create the tables if they don't exist
+Base.metadata.create_all(engine)
+
 
 while 1:
-    with open('/sys/bus/w1/devices/28-800000282522/w1_slave') as device:
-        reading = device.read()
-    temp_data = reading.split('\n')[1].split(" ")[9]
-    temp = float(temp_data[2:]) / 1000
-    print(temp)
+    for name, path in sensors.items():
+        with open(path) as device:
+            reading = device.read()
+        data = reading.split('\n')[1].split(" ")[9]
+        temperature = float(data[2:]) / 1000
+        print(name, temperature)
+        measurement = TemperatureMeasurement(
+            device=name,
+            moment=datetime.now(),
+            value=temperature)
+        # Add to DB session
+        session.add(measurement)
+    session.commit()
 
     time.sleep(1)
