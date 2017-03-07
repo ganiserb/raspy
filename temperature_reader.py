6694855e8c926ff9d4e813de1f30a9651b13eed9
Gabriel <ganiserb@gmail.com>
2017-03-07 00:06:55 -0300

    Persisting to DB every 60 measurements

--- temperature_reader.py
+++ temperature_reader.py
@@ -16,8 +16,9 @@ session = Session()
 # Create the tables if they don't exist
 Base.metadata.create_all(engine)
 
-
+iteration = 0
 while 1:
+    iteration += 1
     for name, path in sensors.items():
         with open(path) as device:
             reading = device.read()
@@ -30,6 +31,11 @@ while 1:
             value=temperature)
         # Add to DB session
         session.add(measurement)
-    session.commit()
 
+    if iteration == 60:
+        # Save to DB every 60 measurements
+        iteration = 0
+        session.commit()
+
+    # Wait for a second before taking the next measurement
     time.sleep(1)
